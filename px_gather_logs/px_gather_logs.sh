d16163085c01a069def00e748bf6331f11f19d67
tjoseph-px <tjoseph+github@purestorage.com>
2025-12-19 10:55:02 +0800

    Update px_gather_logs.sh - Removed migration directory for PX CSI

--- px_gather_logs/px_gather_logs.sh
+++ px_gather_logs/px_gather_logs.sh
@@ -227,7 +227,7 @@ fi
 
 if [[ "$option" == "PX" ]]; then
   if [[ "$PXCSIV3" == "true" ]]; then
-     sub_dir=(${output_dir}/logs/previous ${output_dir}/k8s_px ${output_dir}/k8s_oth ${output_dir}/migration ${output_dir}/k8s_bkp ${output_dir}/k8s_pxb)
+     sub_dir=(${output_dir}/logs/previous ${output_dir}/k8s_px ${output_dir}/k8s_oth ${output_dir}/k8s_bkp ${output_dir}/k8s_pxb)
   else
      sub_dir=(${output_dir}/logs/previous ${output_dir}/px_out ${output_dir}/k8s_px ${output_dir}/k8s_oth ${output_dir}/migration ${output_dir}/k8s_bkp ${output_dir}/k8s_pxb ${output_dir}/storkctl_out)
   fi
