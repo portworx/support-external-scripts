170412f75161f2b953e072035aa5e094b45ebe56
sathish-px <ssengottuvel+github@purestorage.com>
2026-02-13 12:42:56 +0530

    Getting date output from 5 px containers for validating date synchronization
    Getting date output from 5 px containers for validating date synchronization

--- px_gather_logs/px_gather_logs.sh
+++ px_gather_logs/px_gather_logs.sh
@@ -1243,7 +1243,7 @@ for i in "${!log_labels[@]}"; do
             NODE_NAME=$($cli get pods -n "$namespace" "$POD" -o jsonpath='{.spec.nodeName}')
             BASTION_TIME=$(date "+%Y-%m-%d %H:%M:%S")
             POD_TIME=$($cli exec -n "$namespace" "$POD" -- date "+%Y-%m-%d %H:%M:%S" 2>/dev/null)
-            echo "$BASTION_TIME | $POD | $POD_TIME" >> "${output_dir}/k8s_px/date_px_continers.out"
+            echo "[NOT-READY-POD] - $BASTION_TIME | $POD | $POD_TIME" >> "${output_dir}/k8s_px/date_px_continers.out"
             ((date_count++))
           done
 
@@ -1252,7 +1252,7 @@ for i in "${!log_labels[@]}"; do
             NODE_NAME=$($cli get pods -n "$namespace" "$POD" -o jsonpath='{.spec.nodeName}')
             BASTION_TIME=$(date "+%Y-%m-%d %H:%M:%S")
             POD_TIME=$($cli exec -n "$namespace" "$POD" -- date "+%Y-%m-%d %H:%M:%S" 2>/dev/null)
-            echo "$BASTION_TIME | $POD | $POD_TIME" >> "${output_dir}/k8s_px/date_px_continers.out"
+            echo "[READY-POD]    -$BASTION_TIME | $POD | $POD_TIME" >> "${output_dir}/k8s_px/date_px_continers.out"
             ((date_count++))
           done
       fi
