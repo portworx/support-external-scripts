1f9fbda2af03b3d9342f0e7541e28407562c282a
sathish-px <ssengottuvel+github@purestorage.com>
2026-08-03 16:37:08 +0530

    Updating the command for storkctl schedulepolicy
    Updating the command for storkctl schedulepolicy as schedulepolicy is not namespace specific. Providing with namespace is not fetching the output

--- px_gather_logs/px_gather_logs.sh
+++ px_gather_logs/px_gather_logs.sh
@@ -25,7 +25,7 @@
 #
 # ================================================================
 
-SCRIPT_VERSION="26.7.5"
+SCRIPT_VERSION="26.8.0"
 
 
 # Function to display usage
@@ -2165,8 +2165,14 @@ extract_storkctl_op() {
 
         # Run the CLI command and redirect output
        #$cli -n $namespace exec  get "$resource" --all-namespaces > "$output_file"
-       $cli -n $namespace exec service/stork-service -- bash -c "/storkctl/linux/storkctl get "$resource" --all-namespaces" > "$output_file" 2>&1
-    done
+              $cli -n $namespace exec service/stork-service -- bash -c '
+                if [ "$1" = "schedulepolicy" ]; then
+                    /storkctl/linux/storkctl get "$1"
+                else
+                    /storkctl/linux/storkctl get "$1" --all-namespaces
+                fi
+            ' _ "$resource" >"$output_file" 2>&1
+      done
 }
 
 # Generate Cluster_Overview.txt summarising the cluster by parsing files
